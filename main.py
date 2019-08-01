13646a5d763e24a2f975b65ff5dc1120480666a2
Bitiquinho <consoni_2519@hotmail.com>
2019-08-01 14:45:36 -0300

    Restore OpenSim simulation

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ from lqg_prediction_teleoperator import LQGPredTeleoperator
 from lqg_feedback_teleoperator import LQGFFBTeleoperator
 from lqg_feedback_prediction_teleoperator import LQGFFBPredTeleoperator
 
-from simple_plant import SimplePlant
-#import opensim
+#from simple_plant import SimplePlant
+import opensim
 
 from numpy import random, ravel, max, average
 from scipy.signal import butter, lfilter, freqz
 from matplotlib import pyplot
 
-SIM_TIME_STEPS_NUMBER = 1000
+SIM_TIME_STEPS_NUMBER = 5000
 
 USE_DELAY = True#False
+USE_VARIABLE_DELAY = True#False
 USE_DYNAMIC_IMPEDANCE = True#False
 ENVIRONMENT_NAMES = [ "Free Motion", "Resistive", "Assistive"  ]
-ENVIRONMENT_TYPE = 2
+ENVIRONMENT_TYPE = 1
 CONTROLLER_NAMES = [ "PV", "Wave Variables", "LQG", "LQG Prediction", "LQG-FFB", "LQG-FFB Prediction" ]
 CONTROLLER_TYPE = 5
 
@@ -32,7 +33,7 @@ SLAVE_KV = 2.5
 
 NET_TIME_STEP = 0.02
 NET_DELAY_AVG = 0.2 if USE_DELAY else 0.0
-NET_DELAY_VAR = 0.1 if USE_DELAY else 0.0
+NET_DELAY_VAR = 0.1 if ( USE_DELAY and USE_VARIABLE_DELAY ) else 0.0
 
 Teleoperator = PVTeleoperator
 if CONTROLLER_TYPE == 1: Teleoperator = WaveTeleoperator
@@ -60,77 +61,77 @@ b, a = butter( 2, 0.01, btype='low', analog=False )
 setpoints = lfilter( b, a, setpoints )
 
 try:
-  #model = opensim.Model()
+  model = opensim.Model()
   
-  ##model.setUseVisualizer( True )
+  #model.setUseVisualizer( True )
   
-  #model.setName( 'TelerehabSimulator' )
-  #model.setGravity( opensim.Vec3( 0, 0, 0 ) )
+  model.setName( 'TelerehabSimulator' )
+  model.setGravity( opensim.Vec3( 0, 0, 0 ) )
 
-  #master = opensim.Body( 'master', OPERATOR_IMPEDANCE[ 0 ], opensim.Vec3( 0, 0, 0 ), opensim.Inertia( 0, 0, 0 ) )
-  #model.addBody( master )
-  #slave = opensim.Body( 'slave', OPERATOR_IMPEDANCE[ 0 ], opensim.Vec3( 0, 0, 0 ), opensim.Inertia( 0, 0, 0 ) )
-  #model.addBody( slave )
+  master = opensim.Body( 'master', OPERATOR_IMPEDANCE[ 0 ], opensim.Vec3( 0, 0, 0 ), opensim.Inertia( 0, 0, 0 ) )
+  model.addBody( master )
+  slave = opensim.Body( 'slave', OPERATOR_IMPEDANCE[ 0 ], opensim.Vec3( 0, 0, 0 ), opensim.Inertia( 0, 0, 0 ) )
+  model.addBody( slave )
 
-  #ground = model.getGround()
-  #masterToGround = opensim.SliderJoint( 'master2ground', ground, master )
-  #model.addJoint( masterToGround )
-  #slaveToGround = opensim.SliderJoint( 'slave2ground', ground, slave )
-  #model.addJoint( slaveToGround )
+  ground = model.getGround()
+  masterToGround = opensim.SliderJoint( 'master2ground', ground, master )
+  model.addJoint( masterToGround )
+  slaveToGround = opensim.SliderJoint( 'slave2ground', ground, slave )
+  model.addJoint( slaveToGround )
   
-  #blockMesh = opensim.Brick( opensim.Vec3( 0.5, 0.5, 0.5 ) )
-  #blockMesh.setColor( opensim.Red )
-  #masterOffsetFrame = opensim.PhysicalOffsetFrame()
-  #masterOffsetFrame.setParentFrame( master )
-  #masterOffsetFrame.setOffsetTransform( opensim.Transform( opensim.Vec3( 0, 0, 0.5 ) ) )
-  #master.addComponent( masterOffsetFrame )
-  #masterOffsetFrame.attachGeometry( blockMesh.clone() )
-  #blockMesh.setColor( opensim.Blue )
-  #slaveOffsetFrame = opensim.PhysicalOffsetFrame()
-  #slaveOffsetFrame.setParentFrame( slave )
-  #slaveOffsetFrame.setOffsetTransform( opensim.Transform( opensim.Vec3( 0, 0, -0.5 ) ) )
-  #slave.addComponent( slaveOffsetFrame )
-  #slaveOffsetFrame.attachGeometry( blockMesh.clone() )
+  blockMesh = opensim.Brick( opensim.Vec3( 0.5, 0.5, 0.5 ) )
+  blockMesh.setColor( opensim.Red )
+  masterOffsetFrame = opensim.PhysicalOffsetFrame()
+  masterOffsetFrame.setParentFrame( master )
+  masterOffsetFrame.setOffsetTransform( opensim.Transform( opensim.Vec3( 0, 0, 0.5 ) ) )
+  master.addComponent( masterOffsetFrame )
+  masterOffsetFrame.attachGeometry( blockMesh.clone() )
+  blockMesh.setColor( opensim.Blue )
+  slaveOffsetFrame = opensim.PhysicalOffsetFrame()
+  slaveOffsetFrame.setParentFrame( slave )
+  slaveOffsetFrame.setOffsetTransform( opensim.Transform( opensim.Vec3( 0, 0, -0.5 ) ) )
+  slave.addComponent( slaveOffsetFrame )
+  slaveOffsetFrame.attachGeometry( blockMesh.clone() )
   
-  #masterCoordinate = masterToGround.updCoordinate()
-  #masterInputActuator = opensim.CoordinateActuator( 'masterInput' )
-  #masterInputActuator.setCoordinate( masterCoordinate )
-  #model.addForce( masterInputActuator )
-  #masterFeedbackActuator = opensim.CoordinateActuator( 'masterFeedback' )
-  #masterFeedbackActuator.setCoordinate( masterCoordinate )
-  #model.addForce( masterFeedbackActuator )
+  masterCoordinate = masterToGround.updCoordinate()
+  masterInputActuator = opensim.CoordinateActuator( 'masterInput' )
+  masterInputActuator.setCoordinate( masterCoordinate )
+  model.addForce( masterInputActuator )
+  masterFeedbackActuator = opensim.CoordinateActuator( 'masterFeedback' )
+  masterFeedbackActuator.setCoordinate( masterCoordinate )
+  model.addForce( masterFeedbackActuator )
   
-  #slaveCoordinate = slaveToGround.updCoordinate()
-  #slaveInputActuator = opensim.CoordinateActuator( 'slaveInput' )
-  #slaveInputActuator.setCoordinate( slaveCoordinate )
-  #model.addForce( slaveInputActuator )
-  #slaveFeedbackActuator = opensim.CoordinateActuator( 'slaveFeedback' )
-  #slaveFeedbackActuator.setCoordinate( slaveCoordinate )
-  #model.addForce( slaveFeedbackActuator )
+  slaveCoordinate = slaveToGround.updCoordinate()
+  slaveInputActuator = opensim.CoordinateActuator( 'slaveInput' )
+  slaveInputActuator.setCoordinate( slaveCoordinate )
+  model.addForce( slaveInputActuator )
+  slaveFeedbackActuator = opensim.CoordinateActuator( 'slaveFeedback' )
+  slaveFeedbackActuator.setCoordinate( slaveCoordinate )
+  model.addForce( slaveFeedbackActuator )
   
-  ##model.finalizeFromProperties()
-  ##model.printBasicInfo()
+  #model.finalizeFromProperties()
+  #model.printBasicInfo()
   
-  #systemState = model.initSystem()
+  systemState = model.initSystem()
 
-  #masterInputActuator.overrideActuation( systemState, True )
-  #masterFeedbackActuator.overrideActuation( systemState, True )
-  #slaveInputActuator.overrideActuation( systemState, True )
-  #slaveFeedbackActuator.overrideActuation( systemState, True )
+  masterInputActuator.overrideActuation( systemState, True )
+  masterFeedbackActuator.overrideActuation( systemState, True )
+  slaveInputActuator.overrideActuation( systemState, True )
+  slaveFeedbackActuator.overrideActuation( systemState, True )
   
-  #masterCoordinate.setValue( systemState, 0.0 )
-  #slaveCoordinate.setValue( systemState, 0.0 )
+  masterCoordinate.setValue( systemState, 0.0 )
+  slaveCoordinate.setValue( systemState, 0.0 )
 
-  #manager = opensim.Manager( model )
-  #systemState.setTime( 0 )
-  #manager.initialize( systemState )
+  manager = opensim.Manager( model )
+  systemState.setTime( 0 )
+  manager.initialize( systemState )
   
-  referencePlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
-  masterPlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
-  slavePlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
-  referenceOutput = ( 0.0, 0.0, 0.0 )
-  masterOutput = ( 0.0, 0.0, 0.0 )
-  slaveOutput = ( 0.0, 0.0, 0.0 )
+  #referencePlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
+  #masterPlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
+  #slavePlant = SimplePlant( OPERATOR_IMPEDANCE[ 0 ], OPERATOR_IMPEDANCE[ 1 ], OPERATOR_IMPEDANCE[ 2 ], NET_TIME_STEP )
+  #referenceOutput = ( 0.0, 0.0, 0.0 )
+  #masterOutput = ( 0.0, 0.0, 0.0 )
+  #slaveOutput = ( 0.0, 0.0, 0.0 )
   
   #viz = model.updVisualizer().updSimbodyVisualizer()
   #viz.setCameraFieldOfView( opensim.SimTK_PI / 3 )
@@ -171,11 +172,11 @@ try:
     speedSetpoint = ( setpoints[ timeStepIndex ] - setpoints[ timeStepIndex - 1 ] ) / NET_TIME_STEP
     
     # master dynamics
-    #masterOutput = ( masterCoordinate.getValue( systemState ),
-                     #masterCoordinate.getSpeedValue( systemState ),
-                     #masterCoordinate.getAccelerationValue( systemState ) )
+    masterOutput = ( masterCoordinate.getValue( systemState ),
+                     masterCoordinate.getSpeedValue( systemState ),
+                     masterCoordinate.getAccelerationValue( systemState ) )
     masterInput = MASTER_KP * ( setpoint - masterOutput[ 0 ] ) + MASTER_KV * ( speedSetpoint - masterOutput[ 1 ] )
-    #masterInputActuator.setOverrideActuation( systemState, masterInput )
+    masterInputActuator.setOverrideActuation( systemState, masterInput )
     # receive master delayed setpoints
     while simTime >= slaveToMasterTimesQueue[ 0 ]:
       slaveToMasterTimesQueue.pop( 0 )
@@ -184,8 +185,8 @@ try:
     # master control
     slaveFeedback = masterTeleoperator.Process( masterOutput, slaveDelayedOutput, masterInput, slaveDelayedInput, slaveToMasterDelays[ -1 ] )
     slaveFeedbackInput, slaveCorrectedOutput = slaveFeedback
-    #masterFeedbackActuator.setOverrideActuation( systemState, slaveFeedbackInput )
-    masterOutput = masterPlant.Process( masterInput + slaveFeedbackInput )
+    masterFeedbackActuator.setOverrideActuation( systemState, slaveFeedbackInput )
+    #masterOutput = masterPlant.Process( masterInput + slaveFeedbackInput )
     # linearize master system
     masterLinearizer.AddSample( masterOutput[ 0 ], masterOutput[ 1 ], masterOutput[ 2 ], masterInput, slaveFeedbackInput )
     masterInputImpedance, masterOutputImpedance, masterPlantImpedance = masterLinearizer.IdentifySystem( OPERATOR_IMPEDANCE )
@@ -196,13 +197,13 @@ try:
     masterToSlaveTimesQueue.append( simTime + masterToSlaveDelays[ -1 ] )
 
     # slave dynamics
-    #slaveOutput = ( slaveCoordinate.getValue( systemState ),
-                    #slaveCoordinate.getSpeedValue( systemState ),
-                    #slaveCoordinate.getAccelerationValue( systemState ) )
+    slaveOutput = ( slaveCoordinate.getValue( systemState ),
+                    slaveCoordinate.getSpeedValue( systemState ),
+                    slaveCoordinate.getAccelerationValue( systemState ) )
     slaveInput = 0.0
     if ENVIRONMENT_TYPE == 1: slaveInput = - SLAVE_KP * slaveOutput[ 0 ] - SLAVE_KV * slaveOutput[ 1 ]
     elif ENVIRONMENT_TYPE == 2: slaveInput = SLAVE_KP * ( setpoint - slaveOutput[ 0 ] ) + SLAVE_KV * ( speedSetpoint - slaveOutput[ 1 ] )
-    #slaveInputActuator.setOverrideActuation( systemState, slaveInput )
+    slaveInputActuator.setOverrideActuation( systemState, slaveInput )
     # receive slave delayed setpoints
     while simTime >= masterToSlaveTimesQueue[ 0 ]:
       masterToSlaveTimesQueue.pop( 0 )
@@ -211,8 +212,8 @@ try:
     # slave control
     masterFeedback = slaveTeleoperator.Process( slaveOutput, masterDelayedOutput, slaveInput, masterDelayedInput, masterToSlaveDelays[ -1 ] )
     masterFeedbackInput, masterCorrectedOutput = masterFeedback
-    #slaveFeedbackActuator.setOverrideActuation( systemState, masterFeedbackInput )
-    slaveOutput = slavePlant.Process( slaveInput + masterFeedbackInput )
+    slaveFeedbackActuator.setOverrideActuation( systemState, masterFeedbackInput )
+    #slaveOutput = slavePlant.Process( slaveInput + masterFeedbackInput )
     # linearize slave system
     slaveLinearizer.AddSample( slaveOutput[ 0 ], slaveOutput[ 1 ], slaveOutput[ 2 ], slaveInput, masterFeedbackInput )
     slaveInputImpedance, slaveOutputImpedance, slavePlantImpedance = slaveLinearizer.IdentifySystem( OPERATOR_IMPEDANCE )
@@ -222,13 +223,14 @@ try:
     slaveToMasterDelays.append( NET_DELAY_AVG + NET_DELAY_VAR * random.randint( 0, 1000 ) / 1000.0 )
     slaveToMasterTimesQueue.append( simTime + slaveToMasterDelays[ -1 ] )
     
-    referenceInput = MASTER_KP * ( setpoint - referenceOutput[ 0 ] ) + MASTER_KV * ( speedSetpoint - referenceOutput[ 1 ] )
-    referenceFeedbackInput = 0.0
-    if ENVIRONMENT_TYPE == 1: referenceFeedbackInput = - SLAVE_KP * slaveOutput[ 0 ] - SLAVE_KV * slaveOutput[ 1 ]
-    elif ENVIRONMENT_TYPE == 2: referenceFeedbackInput = SLAVE_KP * ( setpoint - referenceOutput[ 0 ] ) + SLAVE_KV * ( speedSetpoint - referenceOutput[ 1 ] )
-    referenceOutput = referencePlant.Process( referenceInput + referenceFeedbackInput )
+    #referenceInput = MASTER_KP * ( setpoint - referenceOutput[ 0 ] ) + MASTER_KV * ( speedSetpoint - referenceOutput[ 1 ] )
+    #referenceFeedbackInput = 0.0
+    #if ENVIRONMENT_TYPE == 1: referenceFeedbackInput = - SLAVE_KP * slaveOutput[ 0 ] - SLAVE_KV * slaveOutput[ 1 ]
+    #elif ENVIRONMENT_TYPE == 2: referenceFeedbackInput = SLAVE_KP * ( setpoint - referenceOutput[ 0 ] ) + SLAVE_KV * ( speedSetpoint - referenceOutput[ 1 ] )
+    #referenceOutput = referencePlant.Process( referenceInput + referenceFeedbackInput )
+    
     # system update
-    #systemState = manager.integrate( simTime )
+    systemState = manager.integrate( simTime )
     
     # perfomance calculation
     positionErrorRMS += ( masterOutput[ 0 ] - slaveOutput[ 0 ] )**2 / SIM_TIME_STEPS_NUMBER
@@ -238,7 +240,7 @@ try:
     
     # data logging
     timeSteps.append( simTime )
-    referencePositions.append( referenceOutput[ 0 ] )
+    #referencePositions.append( referenceOutput[ 0 ] )
     masterPositions.append( masterOutput[ 0 ] )
     slavePositions.append( slaveOutput[ 0 ] )
     masterDelayedPositions.append( masterDelayedOutput[ 0 ] )
@@ -269,11 +271,12 @@ try:
   pyplot.title( 'Teleoperation w/ {} Environment (delay={}±{}[s])\n{} Controller ( position RMS error={:.3f}, impedance RMS error={:.3f} )'.format( ENVIRONMENT_NAMES[ ENVIRONMENT_TYPE ], NET_DELAY_AVG, NET_DELAY_VAR, CONTROLLER_NAMES[ CONTROLLER_TYPE ], positionErrorRMS, impedanceErrorRMS ), fontsize=15 )
   pyplot.ylabel( 'Position [m]', fontsize=10 )
   pyplot.tick_params( axis='x', labelsize=0 )
-  pyplot.plot( timeSteps, referencePositions, 'k:' )
+  #pyplot.plot( timeSteps, referencePositions, 'k:' )
   pyplot.plot( timeSteps, masterDelayedPositions, 'b:', timeSteps, slaveDelayedPositions, 'r:' )
   pyplot.plot( timeSteps, masterSetpointPositions, 'b--', timeSteps, slaveSetpointPositions, 'r--' )
   pyplot.plot( timeSteps, masterPositions, 'b-', timeSteps, slavePositions, 'r-' )
-  pyplot.legend( [ 'reference', 'master-delayed', 'slave-delayed', 'master-setpoint', 'slave-setpoint', 'master', 'slave' ] )
+  #pyplot.legend( [ 'reference', 'master-delayed', 'slave-delayed', 'master-setpoint', 'slave-setpoint', 'master', 'slave' ] )
+  pyplot.legend( [ 'master-delayed', 'slave-delayed', 'master-setpoint', 'slave-setpoint', 'master', 'slave' ] )
   pyplot.subplot( 312, xlim=[ 0.0, SIM_TIME_STEPS_NUMBER * NET_TIME_STEP ], ylim=[ -1.5, 1.5 ] )
   pyplot.ylabel( 'Force [N]', fontsize=10 )
   pyplot.tick_params( axis='x', labelsize=0 )
