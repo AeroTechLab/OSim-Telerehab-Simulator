8862c85ea9940a5593baa53793d39a0b6a7599e8
Bitiquinho <consoni_2519@hotmail.com>
2019-08-01 01:12:08 -0300

    Force feedback controllers

---
+++ lqg_feedback_prediction_teleoperator.py
@@ -0,0 +1,16 @@
+from lqg_controller import LQGController
+from lqg_prediction_controller import LQGPredController
+
+class LQGFFBPredTeleoperator:
+ 
+  def __init__( self, impedance, timeStep ):
+    self.localController = LQGPredController( impedance[ 0 ], impedance[ 1 ], impedance[ 2 ], timeStep )
+  
+  def SetSystem( self, impedance ):
+    self.localController.SetSystem( impedance[ 0 ], impedance[ 1 ], impedance[ 2 ] )
+  
+  def Process( self, localState, remoteState, localForce, remoteForce, timeDelay ):         
+    remoteCorrectedState, remoteCorrectedForce = self.localController.Predict( remoteState, remoteForce, timeDelay )
+    controlForce = self.localController.Process( remoteCorrectedState, localState, remoteCorrectedForce + localForce )    
+    
+    return ( controlForce + remoteCorrectedForce, remoteCorrectedState )
---
+++ lqg_feedback_teleoperator.py
@@ -0,0 +1,14 @@
+from lqg_controller import LQGController
+
+class LQGFFBTeleoperator:
+  
+  def __init__( self, impedance, timeStep ):
+    self.controller = LQGController( impedance[ 0 ], impedance[ 1 ], impedance[ 2 ], timeStep )
+  
+  def SetSystem( self, impedance ):
+    self.controller.SetSystem( impedance[ 0 ], impedance[ 1 ], impedance[ 2 ] )
+  
+  def Process( self, localState, remoteState, localForce, remoteForce, timeDelay ):
+    controlForce = self.controller.Process( remoteState, localState, remoteForce + localForce )
+    
+    return ( controlForce + remoteForce, remoteState )
