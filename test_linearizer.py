ca90cecc39596ec4bee96d1820e65005a0dd70e2
Bitiquinho <consoni_2519@hotmail.com>
2019-11-22 13:23:51 -0300

    Add linearizer test

---
+++ test_linearizer.py
@@ -0,0 +1,30 @@
+import numpy
+
+from system_linearizer import SystemLinearizer
+
+linearizer = SystemLinearizer()
+
+data = numpy.loadtxt( 'samples-2.txt' )
+samplesNumber = data.shape[ 0 ]
+
+inputImpedance = []
+outputImpedance = []
+plantImpedance = []
+sampleImpedance = []
+
+for index in range( samplesNumber - 1 ):
+  force = data[ index, 1 ] + data[ index, 5 ]
+  position = data[ index, 2 ]
+  velocity = data[ index, 3 ]
+  acceleration = data[ index, 4 ]
+  sampleImpedance.append( ( data[ index, 6 ], data[ index, 7 ], data[ index, 8 ] ) )
+  linearizer.AddSample( position, velocity, acceleration, force, 0.0 )
+  impedance = linearizer.IdentifySystem( ( 0.0, 0.0, 0.0 ) )
+  inputImpedance.append( impedance[ 0 ] )
+  outputImpedance.append( impedance[ 1 ] )
+  plantImpedance.append( impedance[ 2 ] )
+  print( impedance[ 0 ], sampleImpedance[ -1 ] )
+  
+print()
+print( numpy.average( inputImpedance, axis=0 ) )
+print( numpy.average( sampleImpedance, axis=0 ) )
